99332ddc03d118b5a754f87664c04a99f033c71c
blessdarah <blessdarahuba@gmail.com>
2021-03-17 05:12:13 +0100

    Update landing page and start base for navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,46 +1,12 @@
- <header class="header header-home-one">
-     <nav class="navbar navbar-default header-navigation stricky">
-         <div class="thm-container clearfix">
-             <!-- Brand and toggle get grouped for better mobile display -->
-             <div class="navbar-header">
-                 <button type="button" class="navbar-toggle collapsed bitmex-icon-menu" data-toggle="collapse"
-                     data-target=".main-navigation" aria-expanded="false"> </button>
-                 <a class="navbar-brand" href="{{ route('welcome') }}">
-                     <img src="img/logo.png" alt="Awesome Image" />
-                 </a>
-             </div>
-
-             <!-- Collect the nav links, forms, and other content for toggling -->
-             <div class="collapse navbar-collapse main-navigation mainmenu " id="main-nav-bar">
-
-                 <ul class="nav navbar-nav navigation-box">
-                     <li class="{{ Config::get('app.page') == 'welcome' ? 'current' : '' }}">
-                         <a href="{{ route('welcome') }}">Home</a>
-                     </li>
-                     <li class="{{ Config::get('app.page') == 'about' ? 'current' : '' }}"> <a
-                             href="{{ route('about-us') }}">About</a></li>
-                     <li class="{{ Config::get('app.page') == 'services' ? 'current' : '' }}">
-                         <a href="{{ route('our-services') }}">Services</a>
-                     </li>
-
-                     <li>
-                         <a href="{{ route('pages.videos') }}"
-                             class="{{ Config::get('app.page') == 'videos' ? 'current' : '' }}">Videos</a>
-                     </li>
-
-                     <li class="{{ Config::get('app.page') == 'blog' ? 'current' : '' }}"><a
-                             href="{{ route('blog') }}">Blog</a>
-                     </li>
-                     <li class="{{ Config::get('app.page') == 'contact' ? 'current' : '' }}"> <a
-                             href="{{ route('contact-us') }}">Contact</a> </li>
-                 </ul>
-             </div><!-- /.navbar-collapse -->
-             <div class="right-side-box">
-                 {{-- <a href="#test-search"
-                     class="search-icon popup-with-zoom-anim bitmex-icon-search"></a> --}}
-                 <a href="#hidden-sidebar" class="side-menu-icon bitmex-icon-menu side-nav-opener">
-                 </a>
-             </div><!-- /.right-side-box -->
-         </div><!-- /.container -->
+ <header class="lg:flex items-center py-4 px-4 lg:px-20">
+     <p id="logo" class="font-bold uppercase text-gray-800 text-center lg:text-left">ReAnalytics</p>
+     {{-- navbar --}}
+     <nav class="flex flex-col space-y-8 lg:flex-row lg:space-y-0 lg:space-x-8 text-center my-8 lg:my-0 lg:ml-16">
+         <a href="#" class="text-gray-600 hover:text-blue-700">{{ __('home') }}</a>
+         <a href="#" class="text-gray-600 hover:text-blue-700">{{ __('about') }}</a>
+         <a href="#" class="text-gray-600 hover:text-blue-700">{{ __('services') }}</a>
+         <a href="#" class="text-gray-600 hover:text-blue-700">{{ __('blog') }}</a>
+         <a href="#" class="text-gray-600 hover:text-blue-700">{{ __('reviews') }}</a>
+         <a href="#" class="text-gray-600 hover:text-blue-700">{{ __('contact') }}</a>
      </nav>
- </header><!-- /.header -->
+ </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-<h3>Welcome to the home page</h3>
+    <div class="container mx-auto lg:w-8/12 bg-gray-100">
+        <h3 class="text-blue-400">Welcome to the home page</h3>
+    </div>
 
 @endsection
-

b55b3c7a94f1a383e3162b46648b1755b0056be2
blessdarah <blessdarahuba@gmail.com>
2021-04-19 20:25:09 +0100

    fix(auth): fix login bug

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.landing')
 
 @section('content')
@@ -6,64 +5,60 @@
 
     <div class="container px-8 mx-auto mt-8 lg:px-36 lg:my-16">
 
-			<form action="{{ route('register') }}" method="POST" class="flex flex-col space-y-4 justify-start md:w-3/6 lg:w-2/6 bg-white rounded shadow-lg px-4 md:px-8 md:py-8 py-6">
-				<h3 class="text-2xl font-semibold text-blue-800 mb-2">{{__('Sign Up')}}</h3>
-				{{-- csrf token --}}
+        <form action="{{ route('register') }}" method="POST"
+            class="flex flex-col justify-start px-4 py-6 space-y-4 bg-white rounded shadow-lg md:w-3/6 lg:w-2/6 md:px-8 md:py-8">
+            <h3 class="mb-2 text-2xl font-semibold text-blue-800">{{ __('Sign Up') }}</h3>
+            {{-- csrf token --}}
 
-				@csrf
+            @csrf
 
-			<div class="form-group">
-					<label for="username" class="block mb-2">
-						{{__('Username')}}
-					</label>
-					@error('username')
-						<div class="bg-red-100 text-sm text-red-700 px-3 py-2 rounded mb-2">{{ $message }}</div>
-					@enderror
-					<input type="text" id="username" name="username"  class="w-full text-gray-500 py-2 bg-gray-100 border-0 px-3 rounded focus:ring-blue-400" value="{{ old('username') ?? '' }}"  />
-				</div>
+            @forelse ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @empty
+                <p>{{ __('No errors found') }}</p>
+            @endforelse
 
-				<div class="form-group">
-					<label for="email" class="block mb-2">
-						{{__('Email address')}}
-					</label>
-					@error('email')
-						<div class="bg-red-100 text-sm text-red-700 px-3 py-2 rounded mb-2">{{ $message }}</div>
-					@enderror
-					<input type="text" id="email" name="email" class="w-full text-gray-500 py-2 bg-gray-100 border-0 px-3 rounded focus:ring-blue-400" value="{{ old('email') ?? '' }}" />
-				</div>
+            <div class="form-group">
+                <label for="username" class="block mb-2">
+                    {{ __('Username') }}
+                </label>
+                @error('name')
+                    <div class="px-3 py-2 mb-2 text-sm text-red-700 bg-red-100 rounded">{{ $message }}</div>
+                @enderror
+                <input type="text" id="username" name="name"
+                    class="w-full px-3 py-2 text-gray-500 bg-gray-100 border-0 rounded focus:ring-blue-400"
+                    value="{{ old('username') ?? '' }}" />
+            </div>
 
-
-				<div class="form-group">
-					<label for="password" class="block mb-2">
-						{{__('Password')}}
-					</label>
-					@error('password')
-						<div class="bg-red-100 text-sm text-red-700 px-3 py-2 rounded mb-2">{{ $message }}</div>
-					@enderror
-					<input type="password" id="password" name="password" class="w-full text-gray-500 py-2 bg-gray-100 border-0 px-3 rounded focus:ring-blue-400" />
-				</div>
-
-				<div class="form-group">
-					<label for="password_confirm" class="block mb-2">
-						{{__('Confirm password')}}
-					</label>
-					@error('password')
-						<div class="bg-red-100 text-sm text-red-700 px-3 py-2 rounded mb-2">{{ $message }}</div>
-					@enderror
-					<input type="password" id="password_confirm" name="password_confirm" class="w-full text-gray-500 py-2 bg-gray-100 border-0 px-3 rounded focus:ring-blue-400" />
-				</div>
+            <div class="form-group">
+                <label for="email" class="block mb-2">
+                    {{ __('Email address') }}
+                </label>
+                @error('email')
+                    <div class="px-3 py-2 mb-2 text-sm text-red-700 bg-red-100 rounded">{{ $message }}</div>
+                @enderror
+                <input type="text" id="email" name="email"
+                    class="w-full px-3 py-2 text-gray-500 bg-gray-100 border-0 rounded focus:ring-blue-400"
+                    value="{{ old('email') ?? '' }}" />
+            </div>
 
 
+            <div class="form-group">
+                <label for="password" class="block mb-2">
+                    {{ __('Password') }}
+                </label>
+                @error('password')
+                    <div class="px-3 py-2 mb-2 text-sm text-red-700 bg-red-100 rounded">{{ $message }}</div>
+                @enderror
+                <input type="password" id="password" name="password"
+                    class="w-full px-3 py-2 text-gray-500 bg-gray-100 border-0 rounded focus:ring-blue-400" />
+            </div>
 
-				<button class="bg-blue-500 focus:bg-opacity-75 active:bg-blue-600 text-blue-100 rounded text-center inline-block py-3 px-4">{{__('Sign up')}}</button>
+            <button
+                class="inline-block px-4 py-3 text-center text-blue-100 bg-blue-500 rounded focus:bg-opacity-75 active:bg-blue-600">{{ __('Sign up') }}</button>
 
-			</form>
-
-
-		</div>
-	</div>
+        </form>
+    </div>
+    </div>
 
 @endsection
-
-
-
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,76 +2,56 @@
 
 
 @section('content')
+    <div class="container px-8 mx-auto mt-8 lg:px-36 lg:mt-16">
+        <div class="grid grid-cols-2 gap-8">
+            <ul class="flex flex-col space-y-6">
+                <li class="flex space-x-4">
+                    <div class="flex items-center justify-center w-20 h-20 text-green-700 bg-green-100">
+                        <i class="bx bx-like bx-md"></i>
+                    </div>
+                    <div class="flex-1">
+                        <p class="mb-2 font-semibold text-gray-800">
+                            {{ __('We are professionals') }}
+                        </p>
+                        <p class="text-gray-600">
+                            {{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et
+                                dolore magna') }}
+                        </p>
+                    </div>
+                </li>
+                <li class="flex space-x-4">
+                    <div class="flex items-center justify-center w-20 h-20 text-green-700 bg-green-100">
+                        <i class="bx bx-face bx-md"></i>
+                    </div>
+                    <div class="flex-1">
+                        <p class="mb-2 font-semibold text-gray-800">
+                            {{ __('We give results') }}
+                        </p>
+                        <p class="text-gray-600">
+                            {{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et
+                                dolore magna') }}
+                        </p>
+                    </div>
+                </li>
+                <li class="flex space-x-4">
+                    <div class="flex items-center justify-center w-20 h-20 text-green-700 bg-green-100">
+                        <i class="bx bx-shield bx-md"></i>
+                    </div>
+                    <div class="flex-1">
+                        <p class="mb-2 font-semibold text-gray-800">
+                            {{ __('We are trusted') }}
+                        </p>
+                        <p class="text-gray-600">
+                            {{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et
+                                dolore magna') }}
+                        </p>
+                    </div>
+                </li>
+            </ul>
 
-    <div class="container px-8 mx-auto mt-8 lg:px-36 lg:my-16">
-
-		<div class="grid grid-cols-2 gap-8">
-
-
-			<ul class="flex flex-col space-y-6">
-
-				<li class="flex space-x-4">
-					<div class="flex items-center justify-center w-20 h-20 text-green-700 bg-green-100">
-						<i class="bx bx-like bx-md"></i>
-						</div>
-						<div class="flex-1">
-						<p class="mb-2 font-semibold text-gray-800">
-							{{ __('Where to find us') }}
-						</p>
-						<p class="text-gray-600">
-							{{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-								sed diam nonumy eirmod tempor invidunt ut labore et
-									dolore magna') }}
-						</p>
-					</div>
-				</li>
-
-				<li class="flex space-x-4">
-					<div class="flex items-center justify-center w-20 h-20 text-green-700 bg-green-100">
-						<i class="bx bx-face bx-md"></i>
-					</div>
-					<div class="flex-1">
-						<p class="mb-2 font-semibold text-gray-800">
-							{{ __('Contact info') }}
-						</p>
-						<p class="text-gray-600">
-							{{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-							sed diam nonumy eirmod tempor invidunt ut labore et
-							dolore magna') }}
-						</p>
-					</div>
-				</li>
-
-			</ul>
-
-			<form action="#" class="flex flex-col space-y-4 justify-start">
-				<h3 class="text-2xl font-semibold text-blue-800 mb-2">{{__('Reach out to us')}}</h3>
-				<div class="form-group">
-					<label for="name" class="block mb-2">
-						{{__('Your name')}}
-					</label>
-					<input type="text" name="name" placeholder="Enter your name" class="w-full text-gray-500 py-2 px-3 rounded focus:ring-blue-400">
-				</div>
-
-				<div class="form-group">
-					<label for="email" class="block mb-2">
-						{{__('Your email address')}}
-					</label>
-					<input type="text" name="email" placeholder="Enter your email" class="w-full text-gray-500 py-2 px-3 rounded focus:ring-blue-400">
-				</div>
-
-				<div class="form-group">
-					<label for="message" class="block mb-2">
-						{{__('Your message')}}
-					</label>
-					<textarea name="message" placeholder="Enter message" class="w-full text-gray-500 py-2 px-3 rounded focus:ring-blue-400"></textarea>
-				</div>
-
-				<button class="bg-blue-500 focus:bg-opacity-75 active:bg-blue-600 text-blue-100 rounded-lg text-center inline-block py-4 px-8">{{__('Submit')}}</button>
-
-			</form>
-
-
-		</div>
-	</div>
+        </div>
+    </div>
 @endsection
